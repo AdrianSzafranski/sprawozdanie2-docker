d02ab9dade133dbd1d30abf8c38c722a08c277f2
PwChO <adszafranski@gmail.com>
2021-11-17 16:20:30 +0100

    edycja do sprawka

--- pluto.sh
+++ pluto.sh
@@ -4,3 +4,9 @@
 date +"Data utworzenia serwera: %d.%m.%Y" > /logi/info.log
 #Zapisanie informacji o ilości dostępnej pamięci do pliku info.log
 cat /proc/meminfo | awk 'NR==3' >> /logi/info.log
+
+#linijka do podpunktu 5 w sprawozdaniu
+cat /logi/info.log
+
+#linijka do podpunktu 6 w sprawozdaniu
+#while true; do sleep 1000; done
