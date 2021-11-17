ce89c04116d058edfe0fd6d2025b507be7a5e720
PwChO <adszafranski@gmail.com>
2021-11-17 16:21:34 +0100

    gotowe

--- pluto.sh
+++ pluto.sh
@@ -6,7 +6,7 @@ date +"Data utworzenia serwera: %d.%m.%Y" > /logi/info.log
 cat /proc/meminfo | awk 'NR==3' >> /logi/info.log
 
 #linijka do podpunktu 5 w sprawozdaniu
-cat /logi/info.log
+#cat /logi/info.log
 
 #linijka do podpunktu 6 w sprawozdaniu
 #while true; do sleep 1000; done
